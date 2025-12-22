34f0196ba8c36c7d93da5abbfcff19f4762f8271
Therajyavardhan <rajyavardhanshuklakks@gmail.com>
2025-12-22 11:06:00 +0530

    Add BST

---
+++ CPP/binarySearchTree/buildBST.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val){
+        data = val;
+        left = right = NULL;
+    }
+};
+
+Node* insert(Node* root,int val){
+    if(!root){
+        root = new Node(val);
+        return root;
+    }
+    
+    if(root->data > val){
+    root->left = insert(root->left,val);
+    return root;
+    }
+    else{ 
+    root->right = insert(root->right,val);
+    return root;
+    }  
+}
+
+Node* buildBST(vector<int> arr){
+    Node* root = NULL;
+
+    for(int val: arr) root = insert(root,val);
+
+    return root;
+}
+
+void inorderTranversal(Node* root){
+    if(!root) return ;
+
+    inorderTranversal(root->left);
+    cout<< root->data <<" ";
+    inorderTranversal(root->right);
+    return ;
+}
+
+int main(){
+    vector<int> arr = {3,2,1,5,4,6};
+
+    Node* root = buildBST(arr);
+    inorderTranversal(root);
+    return 0;
+
+}
